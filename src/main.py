3cd033c7f0122799aa3c11d788fd855d836a24dc
Scott Beru <scottdreu@gmail.com>
2022-09-16 15:03:39 -0700

    Install R packages

--- src/main.py
+++ src/main.py
@@ -1,3 +1,22 @@
+import rpy2
+from rpy2.robjects.packages import importr
+import rpy2.robjects.packages as rpackages
+from rpy2.robjects.vectors import StrVector
+
+import prompt_toolkit
+import tqdm
+
+import sys, os
+
+def install_r_deps():
+    base = importr('base')
+    utils = importr('utils')
+    utils.chooseCRANmirror(ind=1)
+
+    packages_to_install = [x for x in FFRPACKAGES if not rpackages.isinstalled(x)]
+    if len(names_to_install) > 0:
+        utils.install_packages(StrVector(packages_to_install))
+
 def main():
     pass
 
