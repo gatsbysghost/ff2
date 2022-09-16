ae8a3d1975bf342bcd1cdc955601a060a9dfe6de
Scott Beru <scottdreu@gmail.com>
2022-09-16 15:54:21 -0700

    Add RMD-to-R converter

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from rpy2.robjects.packages import importr
 import rpy2.robjects.packages as rpackages
 from rpy2.robjects.vectors import StrVector
 
+from rmdawn import rmdtor
+
 from assets import FFRPACKAGES
 
 import prompt_toolkit
@@ -11,6 +13,7 @@ import tqdm
 import sys, os
 
 def install_r_deps():
+    print("Installing packages...")
     base = importr('base')
     utils = importr('utils')
     utils.chooseCRANmirror(ind=1)
@@ -18,10 +21,16 @@ def install_r_deps():
     packages_to_install = [x for x in FFRPACKAGES if not rpackages.isinstalled(x)]
     if len(packages_to_install) > 0:
         utils.install_packages(StrVector(packages_to_install))
+    print("...done!")
+
+def convert_markdown_to_r():
+    print("Converting RMD (R-Markdown, an R Studio file format) to a regular R file...")
+    rmdtor('../lib/Fantasy-Football-Models/Fantasy Football Analysis v2.Rmd')
 
 def main():
-    print("Installing packages")
     install_r_deps()
+    convert_markdown_to_r()
+
 
 if __name__ == '__main__':
     main()
